f44a2026cc217cf0ca9ce920f2b457c0aa394b2b
boltpius <boltpius1@gmail.com>
2024-02-13 19:54:56 +0100

    made changes to terraform files

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vprofile-eks"
 
   cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3) # using the slice function we get 3 of the az from all the azs mentioned under data source. 
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
